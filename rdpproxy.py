eb05845da5fce639248a8f7a1b7f8a05fec13951
Mitsunori YOSHIDA <marbocub@gmail.com>
2021-06-29 15:28:55 +0000

    refactering

--- rdpproxy.py
+++ rdpproxy.py
@@ -41,78 +41,49 @@ class InfoAPI(threading.Thread):
     def close_socket(self):
         self.info_sock.close()
 
-class DummyTCP(threading.Thread):
-    def __init__(self, client_sock, client_address, client_port):
-        threading.Thread.__init__(self, daemon=True, name="dummy")
-        self.client_sock = client_sock
-        self.client_address = client_address
-        self.client_port = client_port
+class ProxyTCP(threading.Thread):
+    def __init__(self, client):
+        threading.Thread.__init__(self, daemon=True, name="worker")
+        self.client_sock, (self.client_address, self.client_port) = client
+        self.pool = Pool()
+        self.server_sock, (self.server_address, self.server_port) = self.pool.next(self.client_address)
         self.bufsize = 4096
 
     def run(self):
         readfds = set([self.client_sock])
+        if self.server_sock != None:
+            readfds.add(self.server_sock)
         while True:
-            rready, wready, xready = select.select(readfds, [], [], 10)
+            rready, wready, xready = select.select(readfds, [], [])
             for sock in rready:
                 try:
                     msg = sock.recv(self.bufsize)
                     if len(msg) == 0:
-                        self.close_socket()
-                        return
+                        break
+                    peer_sock = self.peer(sock)
+                    if peer_sock != None:
+                        peer_sock.send(msg)
+                    else:
+                        if msg[0:1] == b'\x03':
+                            # sendback disconnect request
+                            tpktHeader = b'\x03\x00\x00\x0b'
+                            x224Ccf = b'\x06\x80\x00\x00\x00\x00\x00'
+                            res = tpktHeader + x224Ccf
+                            sock.send(res)
+                        else:
+                            break
                 except Exception as e:
-                    self.close_socket()
-                    return
+                    break
+            else:
+                continue
+            break
+        self.close_sockets()
 
-    def get_client_address(self):
-        return self.client_address
-
-    def close_socket(self):
-        try:
-            self.client_sock.close()
-        except Exception as e:
-            pass
-
-class ProxyTCP(threading.Thread):
-    def __init__(self, client_sock, client_address, client_port, server_address, server_port):
-        threading.Thread.__init__(self, daemon=True, name="worker")
-        self.client_sock = client_sock
-        self.client_address = client_address
-        self.client_port = client_port
-        self.server_address = server_address
-        self.server_port = server_port
-        self.bufsize = 4096
-        self.pool = Pool()
-        try:
-            self.server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            self.server_sock.connect((server_address, server_port))
-        except Exception as e:
-            raise
-
-    def run(self):
-        readfds = set([self.client_sock, self.server_sock])
-        while True:
-            rready, wready, xready = select.select(readfds, [], [], 10)
-            for sock in rready:
-                if sock is self.server_sock:
-                    try:
-                        msg = sock.recv(self.bufsize)
-                        if len(msg) == 0:
-                            self.close_socket()
-                            return
-                        self.client_sock.send(msg)
-                    except Exception as e:
-                        self.close_socket()
-                        return
-                elif sock is self.client_sock:
-                    try:
-                        msg = sock.recv(self.bufsize)
-                        if len(msg) == 0:
-                            self.close_socket()
-                            return
-                        self.server_sock.send(msg)
-                    except Exception as e:
-                        self.close_socket()
-                        return
+    def peer(self, sock):
+        if sock is self.client_sock:
+            return self.server_sock
+        else:
+            return self.client_sock
 
     def get_server_address(self):
         return self.server_address
@@ -120,8 +91,10 @@ class ProxyTCP(threading.Thread):
     def get_client_address(self):
         return self.client_address
 
-    def close_socket(self):
-        self.client_address = ''
+    def close_sockets(self):
+        server_address = self.server_address
+        self.client_address = None
+        self.server_address = None
         try:
             self.client_sock.close()
         except Exception as e:
@@ -130,15 +103,30 @@ class ProxyTCP(threading.Thread):
             self.server_sock.close()
         except Exception as e:
             pass
-        self.pool.register_client(self.server_address, '')
+        self.pool.release(server_address)
 
 class Pool():
     database = 'db.sqlite3'
 
-    def init_database(self):
+    def __init__(self, server_port = 3389):
+        self.server_port = server_port
         db = sqlite3.connect(self.database)
         db.execute('''create table if not exists pool (server text primary key, weight int, client text, wait_start datetime)''')
         db.close()
+
+    def next(self, client_address):
+        server_address = self.get_server(client_address)
+        server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            server_sock.connect((server_address, self.server_port))
+            self.register_client(server_address, client_address)
+            return server_sock, (server_address, self.server_port)
+        except Exception as e:
+            pass
+            return None, (None, None)
+
+    def release(self, server_address):
+        self.register_client(server_address, '')
 
     def get_server(self, client_address):
         db = sqlite3.connect(self.database)
@@ -199,9 +187,6 @@ class Cleanup(threading.Thread):
             time.sleep(3)
 
 def main():
-    pool = Pool()
-    pool.init_database()
-
     tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     tcp.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1, )
     #udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -215,14 +200,8 @@ def main():
         tcp.bind((host, server_port))
         tcp.listen(5)
         while True:
-            client_sock, (client_address, client_port) = tcp.accept()
-            server_address = pool.get_server(client_address)
-            if server_address == None:
-                th = DummyTCP(client_sock, client_address, client_port)
-                th.start()
-            else:
-                th = ProxyTCP(client_sock, client_address, client_port, server_address, server_port)
-                th.start()
+            th = ProxyTCP(tcp.accept())
+            th.start()
     except Exception as e:
         print(e)
     finally:
